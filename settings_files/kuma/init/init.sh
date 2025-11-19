e0d0f5e931e1fa399d1e90f44bf8e4574373f495
c0936931 <c0936931@mylambton.ca>
2025-11-19 14:37:30 -0500

    Switch to showing status page only

--- settings_files/kuma/init/init.sh
+++ settings_files/kuma/init/init.sh
@@ -4,3 +4,17 @@ sleep 10
 
 # Import monitors from JSON
 node /app/server/import.js /init/monitors.json
+
+sleep 10
+
+# Create a public status page via API
+curl -X POST http://localhost:3001/api/status-page/add \
+  -H "Content-Type: application/json" \
+  -d '{
+    "title": "Public Status",
+    "slug": "default",
+    "theme": "light",
+    "showTags": true,
+    "published": true,
+    "monitors": [1,2]
+  }'
